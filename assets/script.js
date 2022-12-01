119be1d7f9a0a492a4c68e0e086c54afd38b89b1
michelle <113802263+michelletrn@users.noreply.github.com>
2022-11-30 21:47:09 -0500

    starting JS

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var timerEl = document.querySelector("#timer");
+var scoreEl = document.querySelector("#score");
+var startButton = document.querySelector("#start-button");
+
+
+
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+    startTimer();
+}
+
+function startTimer() {
+    var timeLeft = 60;
+    timer = setInterval() {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+    };
+}
